632ccab5228eb4b1c2b12f021758a482d1ce4f6d
WillaimPGallardo <willijuan2011@hotmail.com>
2026-04-12 21:36:12 -0500

    COMMIT 2 — MODELO
    feat: add evento model

--- notificaciones-strategy-observer/src/main/java/com/universidad/notificaciones/modelo/Evento.java
+++ notificaciones-strategy-observer/src/main/java/com/universidad/notificaciones/modelo/Evento.java
@@ -1,5 +1,14 @@
 package com.universidad.notificaciones.modelo;
 
 public class Evento {
-    
+
+    private final String mensaje;
+
+    public Evento(String mensaje) {
+        this.mensaje = mensaje;
+    }
+
+    public String getMensaje() {
+        return mensaje;
+    }
 }
